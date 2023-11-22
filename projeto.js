b7eabfbdc9ff43bcaff0cb7afe6e99db961722ed
saymonvi <saymonvictor2012@icloud.com>
2023-11-22 13:48:23 -0300

    projeto 1 formação lógica DIO

--- projeto.js
+++ projeto.js
@@ -1,2 +1,22 @@
-console.log('Olá mundão')
-console.log('Estou estudando na DIO')
+let nome = ('Dariun')
+let xp = 2001
+let classe = ('')
+console.log('O Héroi ' + nome + ' tem ' + xp + ' de XP')
+if (xp <= 1000) {
+    classe = ('Ferro')
+}else if (xp > 1000 && xp <= 2000){
+    classe = ('Bronze')
+}else if (xp > 2000 && xp <= 6000){
+    classe = ('Prata')
+}else if (xp > 6000 && xp <= 7000){
+    classe = ('Ouro')
+}else if (xp > 7000 && xp <= 8000){
+    classe = ('Platina')
+}else if (xp > 8000 && xp <= 9000){
+    classe = ('Ascendente')
+}else if (xp > 9000 && xp <= 10000){
+    classe = ('Imortal')
+}else if (xp >= 10001){
+    classe = ('Radiante')
+}
+console.log('O Héroi ' + nome + ' se encontra na classe: ' + classe)
